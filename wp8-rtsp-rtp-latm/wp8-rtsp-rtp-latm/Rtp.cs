60ea764c77f342b41a1900a2cce7b2d9550c3537
ahmeda8 <ahmeda8@outlook.com>
2012-12-05 07:02:32 -0800

    Init commit

---
+++ wp8-rtsp-rtp-latm/wp8-rtsp-rtp-latm/Rtp.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Diagnostics;
+
+namespace wp8_rtsp_rtp
+{
+    public class Rtp
+    {
+        private Socket RtpSocket;
+        private SocketAsyncEventArgs RtpEvntArgs;
+        public int ServerPort {get;set;}
+        public string ServerIP {get;set;}
+        public int ClientPort { get; set; }
+        public MemoryStream RtpStream;
+        private bool PortDetermined = false;
+
+        private const string PortDetermineServerAddress = "169.254.80.80";
+        private const int PortDetermineServerPort = 22222;
+        private const int MaxBufferSize = 1024;
+        private const int RtpPacketHeaderSize = 12;
+        private int CurrentPacketSize;
+
+        private static Rtp instance;
+        private AsyncCallback acb;
+
+        private enum RtpState
+        {
+            DeterminePort,
+            Stream
+        }
+
+        private RtpState CurrentState;
+
+        public static Rtp GetInstance()
+        {
+            if (instance == null)
+                instance = new Rtp();
+            return instance;
+        }
+
+        public Rtp()
+        {
+            RtpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            RtpEvntArgs = new SocketAsyncEventArgs();
+            RtpEvntArgs.Completed += RtpEvntArgs_Completed;
+        }
+
+        void RtpEvntArgs_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            switch (e.LastOperation)
+            {
+                case SocketAsyncOperation.SendTo:
+                    Receive();
+                    break;
+                case SocketAsyncOperation.ReceiveFrom:
+                    switch (CurrentState)
+                    {
+                        case RtpState.DeterminePort:
+                            string message = Encoding.UTF8.GetString(e.Buffer, 0, e.BytesTransferred);
+                            ClientPort = int.Parse(message);
+                            PortDetermined = true;
+                            acb.Invoke(new DPIAsyncResult(ClientPort));
+                            break;
+                        case RtpState.Stream:
+                            if (CurrentPacketSize == 0)
+                                CurrentPacketSize = e.BytesTransferred;
+                            if(e.BytesTransferred > RtpPacketHeaderSize) //minimum bytes to accept from rtp packet
+                                RtpStream.Write(e.Buffer, RtpPacketHeaderSize , e.BytesTransferred - RtpPacketHeaderSize);
+                            RtpParser.RTP_HEADER h =  RtpParser.GetHeader(e.Buffer);
+                            if (e.BytesTransferred < CurrentPacketSize)
+                            {
+                                Logging.Log(string.Format("Download Complete {0}",RtpStream.Length));
+                            }
+                            else
+                            {
+                                Logging.Log(string.Format("Continue Downloading , stream size={0} , seqno={1}, ssrc={2}, payload type={3},packet_size={4}",RtpStream.Length,h.seq,h.ssrc,h.pt,e.BytesTransferred));
+                                Receive();
+                            }
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        public void DeterminePort(AsyncCallback callback)
+        {
+            if (PortDetermined)
+                return;
+            acb = new AsyncCallback(callback);
+            RtpEvntArgs.RemoteEndPoint = new IPEndPoint(IPAddress.Parse(PortDetermineServerAddress), PortDetermineServerPort);
+            var send_buffer = Encoding.UTF8.GetBytes("Connect;LoopBack;");
+            RtpEvntArgs.SetBuffer(send_buffer, 0, send_buffer.Length);
+            CurrentState = RtpState.DeterminePort;
+            try
+            {
+                RtpSocket.SendToAsync(RtpEvntArgs);
+            }
+            catch (ObjectDisposedException)
+            {
+                RtpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                RtpSocket.SendToAsync(RtpEvntArgs);
+            }
+
+        }
+
+        public void StartStream()
+        {
+            RtpStream = new MemoryStream();
+            RtpEvntArgs.RemoteEndPoint = new IPEndPoint(IPAddress.Parse(ServerIP), ServerPort);
+            CurrentState = RtpState.Stream;
+            CurrentPacketSize = 0;
+            Receive();
+        }
+
+        private void Receive()
+        {
+
+            try
+            {
+                RtpEvntArgs.SetBuffer(new byte[MaxBufferSize], 0, MaxBufferSize);
+                RtpSocket.ReceiveFromAsync(RtpEvntArgs);
+            }
+            catch (ObjectDisposedException)
+            {
+                Logging.Log("Socket Disposed.");
+            }
+        }
+
+        public void Abort()
+        {
+            RtpSocket.Close();
+            PortDetermined = false;
+        }
+
+    }
+}
